a0a90d435e156c99f775d2fbf83d674f4ce3ded2
Aldo Notebook <aldoaliscioni18@gmail.com>
2022-06-28 11:51:28 -0300

    updated some things

--- 01-Node/homework/bash.js
+++ 01-Node/homework/bash.js
@@ -7,7 +7,7 @@ print('! ');
 // El evento stdin 'data' se dispara cuando el user escribe una línea
 process.stdin.on('data', function (data) {
     
-    let cmd = data.toString().trim().split(' '); // remueve la nueva línea
+    let cmd = data.toString().toLowerCase().trim().split(' '); // remueve la nueva línea
     typeof allCommands[cmd[0]] === 'function' ? allCommands[cmd[0]](cmd) : print('Invalid command\n! ');
 
 });
--- 01-Node/homework/commands/index.js
+++ 01-Node/homework/commands/index.js
@@ -2,50 +2,47 @@ const fs = require('fs');
 const request = require('request');
 
 const done = () => {
-    print('\n! ');
+    process.stdout.write('\n! ');
 }
 
 const print = (data) => {
-    process.stdout.write(data)
+    process.stdout.write(data);
+    done();
 }
 
 const pwd = () => {
     print(require.main.path);
-    done();
 }
 
 const date = () => {
     print(Date());
-    done();
 }
 
 const ls = () => {
     fs.readdir('.', (err, files) => {
         if (err) throw err;
+        let text = ""
         files.forEach((file) => {
-            print(file.toString() + "\n");
+            text = text + file.toString() + "\t"
+
         });
-        done();
+        print(text);
     });
 }
 
 const echo = (data) => {
     data.shift(); // elimino la palabra echo
-    data.forEach(word => {
-        print(word.toString() + ' ');
-    });
-    done();
+    print(data.join(" "));
 }
 
 const cat = (data) => {
     fs.readFile(data[1], 'utf-8', (err, text) => {
         if (err) {
             print(err.toString());
-            done();
             return;
         }
         print(text);
-        done();
+
     });
 }
 
@@ -53,15 +50,12 @@ const head = (data) => {
     fs.readFile(data[1], 'utf-8', (err, text) => {
         if (err) {
             print(err.toString());
-            done();
             return;
         }
-        text = text.split(/\r\n|\r|\n/, -1);
-        for (let i = 0; i <= 10; i++) {
-            const line = text[i].toString();
-            print(line + "\n");
-        }
-        done();
+        const linesToRead = Math.abs(data[2]) || 10
+        text = text.split(/\r\n|\r|\n/, -1).splice(0, linesToRead); // como el text viene en un string, lo separamos en un array por cada salto de linea. Despues, cortamos el array desde 0 hasta las lineas que haya que leer
+        let textToRead = text.reduce((t, line) => t + line + "\n"); // guardamos todas las lineas de texto en un string
+        print(textToRead);
     });
 
 }
@@ -70,32 +64,31 @@ const tail = (data) => {
     fs.readFile(data[1], 'utf-8', (err, text) => {
         if (err) {
             print(err.toString());
-            done();
+
             return;
         }
-        text = text.split(/\r\n|\r|\n/, -1);
-        for (let i = text.length - 1; i >= text.length - 10; i--) {
-            const line = text[i].toString();
-            print(line + "\n");
-        }
-        done();
+        
+        const linesToRead = Math.abs(data[2]) || 10
+        text = text.split(/\r\n|\r|\n/, -1).splice(-linesToRead); // como el text viene en un string, lo separamos en un array por cada salto de linea. Despues, cortamos el array desde 0 hasta las lineas que haya que leer
+        let textToRead = text.reduce((t, line) => t + line + "\n"); // guardamos todas las lineas de texto en un string
+        print(textToRead);
     });
 }
 
 const curl = (info) => {
     if (info.length <= 1) {
         print('Error: Write an URI');
-        done();
+
         return;
     }
     request(info[1], (err, data) => {
         if (err) {
             print(err.toString());
-            done();
+
             return;
         }
         print(data.body)
-        done();
+
     });
 }
 
