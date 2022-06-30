fe22922affc12b40961f005f343aca87288e46bc
Aldo Notebook <aldoaliscioni18@gmail.com>
2022-06-30 10:01:32 -0300

    better code for shoybyname

--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -1,8 +1,6 @@
 var fs = require("fs")
 var http = require("http")
 
-// Escribí acá tu servidor
-
 const urls = ['arcoiris_doge.jpg',
     'badboy_doge.jpg',
     'code_doge.jpg',
@@ -12,17 +10,17 @@ const urls = ['arcoiris_doge.jpg',
 ];
 
 http.createServer((req, res) => {
-    const imgUrl = req.url.split('/')[1];
+    const imgUrl = req.url.split('/', 2)[1];
     const foundedImg = urls.find((img) => img === imgUrl);
 
     if (foundedImg) {
 
+        let img = fs.readFileSync(`${__dirname} + /images/${foundedImg}`);
         res.writeHead(200, { 'Content-Type': 'image/jpeg' });
-        let img = fs.readFileSync(__dirname + `/images/${foundedImg}`);
         res.end(img);
     } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('Imagen no encontrada =(');
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end('<p>404 - Image not found =(</p>');
     }
 
 }).listen(1337, '127.0.0.1');
