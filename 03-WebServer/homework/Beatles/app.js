b546e2c2db2f78239cb13d925f6a86017cf06051
Aldo Notebook <aldoaliscioni18@gmail.com>
2022-06-30 00:16:10 -0300

    finished homework webserver

--- 03-WebServer/homework/Beatles/app.js
+++ 03-WebServer/homework/Beatles/app.js
@@ -1,24 +1,53 @@
-var http = require('http');
-var fs   = require('fs');
+let http = require("http");
+let fs = require("fs");
 
-var beatles=[{
-  name: "John Lennon",
-  birthdate: "09/10/1940",
-  profilePic:"https://blogs.correiobraziliense.com.br/trilhasonora/wp-content/uploads/sites/39/2020/10/CBNFOT081020100047-550x549.jpg"
-},
-{
-  name: "Paul McCartney",
-  birthdate: "18/06/1942",
-  profilePic:"http://gazettereview.com/wp-content/uploads/2016/06/paul-mccartney.jpg"
-},
-{
-  name: "George Harrison",
-  birthdate: "25/02/1946",
-  profilePic:"https://canaldosbeatles.files.wordpress.com/2012/02/george-george-harrison-8321345-438-600.jpg"
-},
-{
-  name: "Richard Starkey",
-  birthdate: "07/08/1940",
-  profilePic:"http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg"
+const beatles = require("./data");
+
+
+const replaceStrings = (str, data) => {
+  data.forEach(replace => {
+    str.replace(`/\${${replace}}`)
+  });
 }
-]
+
+http
+  .createServer((req, res) => {
+    const data = req.url.split("/")[2] || req.url.split("/")[1];
+    let searchedBeatleIndex = -1;
+    if (data)
+      searchedBeatleIndex = beatles.members.findIndex(
+        (b) => b.name === data.replace("%20", " ")
+      );
+
+    switch (req.url) {
+      case "/":
+        res.writeHead(200, { "Content-Type": "text/html" });
+        const html = fs.readFileSync("./index.html", "utf-8");
+        res.end(html);
+        break;
+      // case pagina personal del beatle 
+      case (searchedBeatleIndex >= 0) ? `/${beatles.members[searchedBeatleIndex].name.replace(" ", "%20")}` : undefined:
+        res.writeHead(200, { "Content-Type": "text/html" });
+        const actualBeatle = beatles.members[searchedBeatleIndex];
+        const htmlBeatle = fs.readFileSync("./beatle.html", "utf-8")
+          .replace(/\${name}/g, actualBeatle.name)
+          .replace(/\${dateBirth}/g, actualBeatle.birthdate)
+          .replace(/\${sourceImg}/g, actualBeatle.profilePic);
+        res.end(htmlBeatle);
+        break;
+      case "/api":
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(beatles));
+        break;
+      case (searchedBeatleIndex >= 0) ? `/api/${beatles.members[searchedBeatleIndex].name.replace(" ", "%20")}` : undefined:
+        res.writeHead(200, { "Content-Type": "application/json" });
+
+        res.end(JSON.stringify(beatles.members[searchedBeatleIndex]));
+        break;
+      default:
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("404 Page Don't Found");
+        break;
+    }
+  })
+  .listen(1337, "127.0.0.1");
--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -1,4 +1,28 @@
-var fs  = require("fs")
-var http  = require("http")
+var fs = require("fs")
+var http = require("http")
 
 // Escribí acá tu servidor
+
+const urls = ['arcoiris_doge.jpg',
+    'badboy_doge.jpg',
+    'code_doge.jpg',
+    'resaca_doge.jpg',
+    'retrato_doge.jpg',
+    'sexy_doge.jpg'
+];
+
+http.createServer((req, res) => {
+    const imgUrl = req.url.split('/')[1];
+    const foundedImg = urls.find((img) => img === imgUrl);
+
+    if (foundedImg) {
+
+        res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+        let img = fs.readFileSync(__dirname + `/images/${foundedImg}`);
+        res.end(img);
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Imagen no encontrada =(');
+    }
+
+}).listen(1337, '127.0.0.1');
