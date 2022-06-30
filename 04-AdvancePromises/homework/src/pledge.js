78fc5da132ce1e37e80d2d286241df90958f5520
Aldo Notebook <aldoaliscioni18@gmail.com>
2022-06-30 16:38:02 -0300

    finished first chapter of advanced promises homework

--- 04-AdvancePromises/homework/src/pledge.js
+++ 04-AdvancePromises/homework/src/pledge.js
@@ -5,6 +5,34 @@ Promises Workshop: construye la libreria de ES6 promises, pledge.js
 // // TU CÓDIGO AQUÍ:
 
 
+class $Promise {
+
+    constructor(executor) {
+        if (typeof executor !== 'function') throw new TypeError('executor is not a function');
+        this._state = 'pending';
+        this._value = undefined
+
+        executor(this._internalResolve.bind(this), this._internalReject.bind(this));
+    }
+
+    _internalResolve(info) {
+        if (this._state === 'pending') {
+            this._value = info;
+            this._state = 'fulfilled';
+        }
+    }
+
+    _internalReject(reason) {
+        if (this._state === 'pending') {
+            this._value = reason;
+            this._state = 'rejected';
+        }
+    }
+
+}
+
+
+
 
 module.exports = $Promise;
 /*-------------------------------------------------------
