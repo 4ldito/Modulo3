d98bed32bca94d7f121e2e7abfa6620f81e91bf4
Aldo Notebook <aldoaliscioni18@gmail.com>
2022-07-03 20:02:17 -0300

    finished all advanced promises homework

--- 04-AdvancePromises/homework/src/pledge.js
+++ 04-AdvancePromises/homework/src/pledge.js
@@ -4,104 +4,179 @@ Promises Workshop: construye la libreria de ES6 promises, pledge.js
 ----------------------------------------------------------------*/
 // // TU CÓDIGO AQUÍ:
 
-
-function $Promise(executor) {
-    if (typeof executor !== 'function') throw new TypeError('executor is not a function');
-    this._state = 'pending';
-    this._value = undefined;
-    this._handlerGroups = [];
-    //esto se puede hacer con .bind o haciedno una función flecha para indicar que el this hace referencia a dónde fue declarada.
-    executor((value) => this._internalResolve(value), this._internalReject.bind(this));
-}
-
-$Promise.prototype.then = function (onFulfilled, onRejected) {
-    const downstreamPromise = new $Promise(function () { });
-    this._handlerGroups.push({
-        successCb: (typeof onFulfilled === 'function') ? onFulfilled : false,
-        errorCb: (typeof onRejected === 'function') ? onRejected : false,
-        downstreamPromise
-    });
-    if (this._state !== 'pending') this._callHandlers();
-
-    return downstreamPromise;
-}
-
-$Promise.prototype.catch = function (error) {
-    return this.then(null, error);
-}
-
-$Promise.prototype._internalResolve = function (info) {
-    if (this._state === 'pending') {
-        this._value = info;
-        this._state = 'fulfilled';
-        this._callHandlers();
-    }
-}
-
-$Promise.prototype._internalReject = function (reason) {
-    if (this._state === 'pending') {
-        this._value = reason;
-        this._state = 'rejected';
-        this._callHandlers();
-    }
-}
-
-$Promise.prototype._callHandlers = function () {
-    if ((this._state !== 'pending') && (this._handlerGroups.length > 0)) {
-        // Copiamos la variable handlerGroups para una mejor lectura
-        const groups = this._handlerGroups;
-        if (this._state === 'rejected') {
-            // const indexError = groups.findIndex((g) => g.errorCb !== false);
-            // if (indexError >= 0) groups[indexError].errorCb(this._value);
-            // groups.forEach(group => {
-            groups.forEach(({ errorCb, downstreamPromise }) => {
-                if (errorCb) {
-                    try {
-                        const result = errorCb(this._value);
-                        if (result instanceof $Promise) {
-                            result.then(
-                                (value) => downstreamPromise._internalResolve(value),
-                                (value) => downstreamPromise._internalReject(value)
-                            );
-                        } else {
-                            downstreamPromise._internalResolve(result);
-                        }
-                    } catch (error) {
-                        downstreamPromise._internalReject(error);
-                    }
-
-                } else {
-                    downstreamPromise._internalReject(this._value);
-                }
-            });
-        } else { // Si el estado de la promesa es fulfilled: 
-            // groups.forEach(group => {
-            groups.forEach(({ successCb, downstreamPromise }) => {
-                if (successCb) { //si el then tiene un succesCb
-                    try {
-                        const result = successCb(this._value);
-                        if (result instanceof $Promise) {
-                            result.then(
-                                (value) => downstreamPromise._internalResolve(value),
-                                (value) => downstreamPromise._internalReject(value)
-                            );
-                        } else {
-                            downstreamPromise._internalResolve(result);
-                        }
-                    } catch (error) {
-                        downstreamPromise._internalReject(error);
-                    }
-                } else { // si el then no tiene un succesCb
-                    downstreamPromise._internalResolve(this._value)
-                }
-            });
-        }
+class $Promise {
+    constructor(executor) {
+        if (typeof executor !== 'function')
+            throw new TypeError('executor is not a function');
+        this._state = 'pending';
+        this._value = undefined;
         this._handlerGroups = [];
+        //esto se puede hacer con .bind o haciedno una función flecha para indicar que el this hace referencia a dónde fue declarada.
+        executor((value) => this._internalResolve(value), this._internalReject.bind(this));
+    }
+
+    static resolve(data) {
+        if (data instanceof $Promise) return data;
+        const promise = new $Promise(() => { });
+        promise._internalResolve(data)
+        return promise;
+    }
+
+    static all(promises) {
+        if (!Array.isArray(promises)) throw TypeError('promises should be an array');
+        //const promise = new $Promise(()=> {});
+        // const newData = promises.map((element) => {
+        //     if (element instanceof $Promise) {
+        //         return element._value;
+        //     } else {
+        //         return element
+        //     }
+        // });
+        // Esta linea es lo mismo q lo de arriba pero mas simplificado.
+        //const newData = promises.map((element) => element instanceof $Promise ? element._value : element);
+        //promise._internalResolve(newData);
+        const p = new $Promise((resolve, reject) => {
+            const totalPromises = promises.length;
+            let resolvedPromises = 0;
+            const results = new Array(totalPromises);
+            promises.forEach((promise, index) => {
+                if (promise instanceof $Promise) {
+                    promise.then((value) => {
+                        resolvedPromises++;
+                        results[index] = value;
+                        console.log('P Agregando el value', value);
+                        console.log('P results despues de insertar', results);
+                        if (totalPromises === resolvedPromises) resolve(results);
+                    }, reject);
+                } else {
+                    resolvedPromises++;
+                    results[index] = promise;
+                    console.log('P Agregando el elemento', promise);
+                    console.log('P results despues de insertar', results);
+                    if (totalPromises === resolvedPromises) resolve(results);
+                }
+            })
+        });
+        const promise = new $Promise((resolve, reject) => {
+            // const newData = promises.map((element) =>  element instanceof $Promise ? element._value : element);
+            //const totalPromises = promises.length;
+            //let resolvedPromises = 0;
+            //const newData = new Array(totalPromises);
+            const newData = [];
+            promises.forEach((element, i) => {
+                if (element instanceof $Promise) {
+                    element.then(function (value) {
+                        //resolvedPromises++
+                        newData[i] = value;
+                        console.log('PROMISE Agregando el value', value);
+                        console.log('PROMISE newData despues de insertar', newData);
+                        if (newData.length === promises.length) resolve(newData);
+                    }, reject)
+                } else {
+                    //resolvedPromises++;
+                    newData[i] = element;
+                    console.log('PROMISE Agregnado el elemento', element);
+                    console.log('PROMISE newData despues de insertar', newData);
+                    if (newData.length === promises.length) resolve(newData);
+                }
+            });
+
+        });
+        // Por alguna razon no funciona con promise, pero si con P cuando hacen practicamente el mismo comportamiento. 
+        console.log('p', p);
+        console.log('promise', promise);
+        console.log('/////////////');
+        return p;
+    }
+    then(onFulfilled, onRejected) {
+        const downstreamPromise = new $Promise(function () { });
+        this._handlerGroups.push({
+            successCb: (typeof onFulfilled === 'function') ? onFulfilled : false,
+            errorCb: (typeof onRejected === 'function') ? onRejected : false,
+            downstreamPromise
+        });
+        if (this._state !== 'pending')
+            this._callHandlers();
+
+        return downstreamPromise;
+    }
+
+    catch(error) {
+        return this.then(null, error);
+    }
+
+    _internalResolve(info) {
+        if (this._state === 'pending') {
+            this._value = info;
+            this._state = 'fulfilled';
+            this._callHandlers();
+        }
+    }
+
+    _internalReject(reason) {
+        if (this._state === 'pending') {
+            this._value = reason;
+            this._state = 'rejected';
+            this._callHandlers();
+        }
+    }
+
+    _callHandlers() {
+        if ((this._state !== 'pending') && (this._handlerGroups.length > 0)) {
+            // Copiamos la variable handlerGroups para una mejor lectura
+            const groups = this._handlerGroups;
+            if (this._state === 'rejected') {
+                // const indexError = groups.findIndex((g) => g.errorCb !== false);
+                // if (indexError >= 0) groups[indexError].errorCb(this._value);
+                // groups.forEach(group => {
+                groups.forEach(({ errorCb, downstreamPromise }) => {
+                    if (errorCb) {
+                        try {
+                            const result = errorCb(this._value);
+                            if (result instanceof $Promise) {
+                                result.then(
+                                    (value) => downstreamPromise._internalResolve(value),
+                                    (value) => downstreamPromise._internalReject(value)
+                                );
+                            } else {
+                                downstreamPromise._internalResolve(result);
+                            }
+                        } catch (error) {
+                            downstreamPromise._internalReject(error);
+                        }
+
+                    } else {
+                        downstreamPromise._internalReject(this._value);
+                    }
+                });
+            } else { // Si el estado de la promesa es fulfilled: 
+                // groups.forEach(group => {
+                groups.forEach(({ successCb, downstreamPromise }) => {
+                    if (successCb) { //si el then tiene un succesCb
+                        try {
+                            const result = successCb(this._value);
+                            if (result instanceof $Promise) {
+                                result.then(
+                                    (value) => downstreamPromise._internalResolve(value),
+                                    (value) => downstreamPromise._internalReject(value)
+                                );
+                            } else {
+                                downstreamPromise._internalResolve(result);
+                            }
+                        } catch (error) {
+                            downstreamPromise._internalReject(error);
+                        }
+                    } else { // si el then no tiene un succesCb
+                        downstreamPromise._internalResolve(this._value);
+                    }
+                });
+            }
+            this._handlerGroups = [];
+        }
     }
 }
 
 module.exports = $Promise;
-
 
 // class $Promise {
 //     constructor(executor) {
--- 04-AdvancePromises/homework/test/pledge.spec.ch5.js
+++ 04-AdvancePromises/homework/test/pledge.spec.ch5.js
@@ -42,15 +42,15 @@ function noop () {}
 
 describe('El método estático `$Promise.resolve`', function(){
 
-  xit('es una función, y no una que ya hemos escrito', function(){
+  it('es una función, y no una que ya hemos escrito', function(){
     expect( typeof $Promise.resolve ).toBe( 'function' );
-    var promise = new $Promise(noop);
+    let promise = new $Promise(noop);
     expect( $Promise.resolve ).not.toBe( promise._internalResolve );
   });
 
-  xit('toma un <valor plano A> y devuelve una <promesa para A>', function(){
+  it('toma un <valor plano A> y devuelve una <promesa para A>', function(){
     [42, 'hi', {}, undefined, /cool/, false].forEach(value => {
-      var promise = $Promise.resolve(value);
+      let promise = $Promise.resolve(value);
       expect( promise instanceof $Promise ).toBe( true );
       // No setea el estado y valor manualmente; llama un resolver.
       expect( promise._state ).toBe( 'fulfilled' );
@@ -60,9 +60,9 @@ describe('El método estático `$Promise.resolve`', function(){
 
   // Esto sería mas complejo con "then"s, pero vamos a ignorar eso.
 
-  xit('toma una <promesa para A> y devuelve la misma <promesa para A>', function(){
-    var firstPromise = new $Promise(noop);
-    var secondPromise = $Promise.resolve(firstPromise);
+  it('toma una <promesa para A> y devuelve la misma <promesa para A>', function(){
+    let firstPromise = new $Promise(noop);
+    let secondPromise = $Promise.resolve(firstPromise);
     expect( secondPromise ).toBe( firstPromise );
   });
 
@@ -74,12 +74,12 @@ describe('El método estático `$Promise.resolve`', function(){
   // Este spec debería ya funcionar si lo de arriba funciona.
   // Lee a través de las assertions y tratá de entender que demuestran.
 
-  xit('demuestra porque "resolved" y "fulfilled" no son sinónimos', function(){
-    var rejectedPromise = new $Promise(function (resolve, reject) {
+  it('demuestra porque "resolved" y "fulfilled" no son sinónimos', function(){
+    let rejectedPromise = new $Promise(function (resolve, reject) {
       reject();
     });
     // Y ahora la revelación:
-    var result = $Promise.resolve(rejectedPromise); // RESOLVIENDO...
+    let result = $Promise.resolve(rejectedPromise); // RESOLVIENDO...
     expect( result._state ).toBe( 'rejected' ); // ...pero RECHAZADA!
     // Hemos "resuelto" pero igual terminado con una promesa rechazada.
     // Asi que "resolve" realmente significa *intento* de completarla(fulfill).
@@ -96,16 +96,16 @@ describe('El método estático `$Promise.resolve`', function(){
 
 describe('El método estático `$Promise.all`', function(){
 
-  var SMALL_DELAY = 10;
-  var MAX_DELAY = 100;
+  let SMALL_DELAY = 10;
+  let MAX_DELAY = 100;
 
-  var values;
+  let values;
   beforeEach(function(){
     values = [42, 'hi', false, {}, undefined, [] ];
     jasmine.addMatchers(customMatchers);
   });
 
-  xit('es una función', function(){
+  it('es una función', function(){
     expect( typeof $Promise.all ).toBe( 'function' );
   });
 
@@ -113,7 +113,7 @@ describe('El método estático `$Promise.all`', function(){
   // [iterable](https://mzl.la/1SopN1G), pero esto va mas alla del
   // scope de Pledge. Nuestro `.all` solo necesita soportar arreglos.
 
-  xit('toma un solo argumento de un arreglo', function(){
+  it('toma un solo argumento de un arreglo', function(){
     // Pasando un arreglo dentro de `$Promise.all` causa ningún error.
     function callingAllWithArrays () {
       $Promise.all([]);
@@ -130,28 +130,28 @@ describe('El método estático `$Promise.all`', function(){
 
   // No parece tán dificil al principio.
 
-  xit('convierte un <arreglo de valores> a una <promesa para un arreglo de valores>', function (done) {
-    var promise = $Promise.all(values);
+  it('convierte un <arreglo de valores> a una <promesa para un arreglo de valores>', function (done) {
+    let promise = $Promise.all(values);
     expect( promise instanceof $Promise ).toBe(true);
     // La promesa debería completarse con los valores
     expect( promise ).toFulfillWith( values, done );
   });
 
   // Uh oh, se va volviendo un poco más difícil
-  xit('convierte un <arreglo de promesas> en una <promesa para un arreglo de valores>', function (done) {
-    var promises = values.map(value => $Promise.resolve(value));
-    var promise = $Promise.all(promises);
+  it('convierte un <arreglo de promesas> en una <promesa para un arreglo de valores>', function (done) {
+    let promises = values.map(value => $Promise.resolve(value));
+    let promise = $Promise.all(promises);
     // La promesa debería completarse con valores (no promesas por valores).
     expect( promise ).toFulfillWith( values, done );
   });
 
   // Sin atajos; cada elemento individual puede ser un valor o una promesa por un valor.
 
-  xit('convierte un <arreglo de valores y promesas> a una <promesa para un arreglo de valores>', function (done) {
-    var valuesAndPromises = values.map(value => {
+  it('convierte un <arreglo de valores y promesas> a una <promesa para un arreglo de valores>', function (done) {
+    let valuesAndPromises = values.map(value => {
       return Math.random() < 0.5 ? value : $Promise.resolve(value);
     });
-    var promise = $Promise.all(valuesAndPromises);
+    let promise = $Promise.all(valuesAndPromises);
     // la promesa debería completarse con valores (no una mezcla de promesas y valores).
     expect( promise ).toFulfillWith( values, done );
   });
@@ -172,11 +172,11 @@ describe('El método estático `$Promise.all`', function(){
   //acceder al eventual valor de la promesa? Vas a necesitar alterar
   // o aumentar tu enfoque aquí.
 
-  xit('convierte un <arreglo de promesas async> en una <promesa para un arreglo de valores>', function (done) {
-    var promises = values.map((value, i) => {
+  it('convierte un <arreglo de promesas async> en una <promesa para un arreglo de valores>', function (done) {
+    let promises = values.map((value, i) => {
       return slowPromise(value, SMALL_DELAY * (i + 1));
     });
-    var promise = $Promise.all(promises);
+    let promise = $Promise.all(promises);
     // la promesa debería completarse con valores…
     // …una vez que esos valores existen
     expect( promise ).toFulfillWith( values, done );
@@ -186,9 +186,9 @@ describe('El método estático `$Promise.all`', function(){
   // De alguna manera tenés que mantener rastro de que valores
   // van donde en el arreglo final
 
-  xit('convierte un <arreglo de promesas async> (completandose en orden random) en una <promesa para un arreglo de valores> (ordenadas por el index del arreglo original)', function (done) {
-    var promises = values.map(slowPromise); // delays random
-    var promise = $Promise.all(promises);
+  it('convierte un <arreglo de promesas async> (completandose en orden random) en una <promesa para un arreglo de valores> (ordenadas por el index del arreglo original)', function (done) {
+    let promises = values.map(slowPromise); // delays random
+    let promise = $Promise.all(promises);
     // la promesa debería completarse con valores, y en el
     // orden correcto también!
     expect( promise ).toFulfillWith( values, done );
@@ -196,41 +196,41 @@ describe('El método estático `$Promise.all`', function(){
 
   // Tan cerca ahora! ¿Qué pasa si una de las promesas falla?
 
-  xit('rechaza con <razon E> cuando uno de las promesas ingresadas rechaza con <razon E>', function (done) {
+  it('rechaza con <razon E> cuando uno de las promesas ingresadas rechaza con <razon E>', function (done) {
     // la promesa que rechaza luego de un random delay
-    var promiseThatRejects = new $Promise(noop);
-    var doomsday = Math.random * MAX_DELAY;
+    let promiseThatRejects = new $Promise(noop);
+    let doomsday = Math.random * MAX_DELAY;
     setTimeout(() => promiseThatRejects._internalReject('any Black Mirror episode'), doomsday);
     // un par de promesas que se van a completar en orden random
-    var promises = values.map(slowPromise);
+    let promises = values.map(slowPromise);
     // mandamos nuestra promesa condenada en algún lugar
-    var randomIndex = Math.floor(Math.random() * promises.length);
+    let randomIndex = Math.floor(Math.random() * promises.length);
     promises.splice(randomIndex, 0, promiseThatRejects);
     // espera por todo con $Promise.all
-    var promise = $Promise.all(promises);
+    let promise = $Promise.all(promises);
     // la promesa debería ser rechazada
     expect( promise ).toRejectWith( 'any Black Mirror episode', done );
   });
 
   // This probably already passes, but let's be sure. We're strict that way.
 
-  xit('no es afectado por rechazos adicionales', function (done) {
+  it('no es afectado por rechazos adicionales', function (done) {
     // las promesas que rechaza luego de un random delay
-    var doomed = new $Promise(noop);
-    var reallyDoomed = new $Promise(noop);
-    var doomsday = Math.random * MAX_DELAY;
-    var postApocalypse = doomsday + SMALL_DELAY;
+    let doomed = new $Promise(noop);
+    let reallyDoomed = new $Promise(noop);
+    let doomsday = Math.random * MAX_DELAY;
+    let postApocalypse = doomsday + SMALL_DELAY;
     setTimeout(() => doomed._internalReject('I am the first rejection'), doomsday);
     setTimeout(() => reallyDoomed._internalReject('I am too late, ignore me'), postApocalypse);
     // un par de promesas que se completan en orden random
-    var promises = values.map(slowPromise);
+    let promises = values.map(slowPromise);
     // mandamos nuestras promesas condenadas en algún lugar
-    var randomIndex1 = Math.floor(Math.random() * promises.length);
-    var randomIndex2 = Math.floor(Math.random() * promises.length);
+    let randomIndex1 = Math.floor(Math.random() * promises.length);
+    let randomIndex2 = Math.floor(Math.random() * promises.length);
     promises.splice(randomIndex1, 0, doomed);
     promises.splice(randomIndex2, 0, reallyDoomed);
     // espera por todo con $Promise.all
-    var promise = $Promise.all(promises);
+    let promise = $Promise.all(promises);
     // la promesa debería ser rechazada con la primera razón.
     expect( promise ).toRejectWith( 'I am the first rejection', done );
   });
